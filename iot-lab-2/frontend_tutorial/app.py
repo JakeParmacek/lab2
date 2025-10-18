a6b0c0ae2cde2f33ea317c04da1f484565934b6c
Jake Parmacek <jacobp7@illinois.edu>
2025-10-18 15:50:32 -0500

    new

--- iot-lab-2/frontend_tutorial/app.py
+++ iot-lab-2/frontend_tutorial/app.py
@@ -23,4 +23,4 @@ def index():
     return jsonify(server_greet = greeting)
 
 if __name__ == '__main__':
-    app.run( host='192.168.88.204', port = 5000, debug=True)
+    app.run( host='192.168.88.204', port = 65432, debug=True)

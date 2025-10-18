5f8d6c493715e7dc8cd1273cf5ed03f95fe18a83
Jake Parmacek <jacobp7@illinois.edu>
2025-10-18 14:18:45 -0500

    new IP

--- frontend_tutorial/wifi_client.py
+++ frontend_tutorial/wifi_client.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = "192.168.2.2" # IP address of your Raspberry PI
+HOST = "10.183.1.15" # IP address of your Raspberry PI
 PORT = 65432          # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
--- frontend_tutorial/wifi_server.py
+++ frontend_tutorial/wifi_server.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = "192.168.2.2" # IP address of your Raspberry PI
+HOST = "10.183.1.15" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:

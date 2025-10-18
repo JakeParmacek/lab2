40f39901db731ca81c8356253f367711e1ed90e3
Jake Parmacek <jacobp7@illinois.edu>
2025-10-18 16:10:31 -0500

    working electron

--- frontend_tutorial/app.py
+++ frontend_tutorial/app.py
@@ -23,4 +23,4 @@ def index():
     return jsonify(server_greet = greeting)
 
 if __name__ == '__main__':
-    app.run( host='192.168.88.204', port = 5000, debug=True)
+    app.run( host='192.168.88.52', port = 5000, debug=True)
